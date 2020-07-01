c468db8f552afebd36803c462479e9e80c621392
Андрейс Жестянниковс <azhestyannikovs@edu.hse.ru>
2020-07-01 12:06:38 +0300

    version1

--- PuzzleBlock.cs
+++ PuzzleBlock.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Puzzle15
 {
-    class PuzzleBlock
+    class PuzzleBlock : Button
     {
-    
+        public PuzzleBlock()
+        {
+            InitializePuzzleBlock();
+        }
+        private void InitializePuzzleBlock()
+        {
+            this.BackColor = Color.Orange;
+            this.Height = 80;
+            this.Width = 80;
+            this.Text = "0";
+            this.Font = new Font("Roboto", 18);
+        }
     }
 }
